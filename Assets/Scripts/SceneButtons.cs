74bc59035de8741c44030fad4203729497a4ba09
Adam Snyder <adam555snyder@gmail.com>
2022-10-11 20:21:15 -0500

    adds scene canvas with main menu button and resetpos button. not working in RECVX scene

---
+++ Assets/Scripts/SceneButtons.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARFoundation;
+
+public class SceneButtons : MonoBehaviour
+{
+    [SerializeField] ARSession arSession;
+    [SerializeField] Camera arCamera;
+
+
+    private void Awake()
+    {
+        arCamera = Camera.main;
+        arSession = arCamera.transform.parent.Find("AR Session").GetComponent<ARSession>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ResetPosButton()
+    {
+        arCamera.transform.localPosition = new Vector3(0, 0, 0);
+        arSession.Reset();
+    }
+}
