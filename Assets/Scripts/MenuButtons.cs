166e48b4d623baff902871794567a15ff36ff2a5
Adam Snyder <adam555snyder@gmail.com>
2022-11-03 00:19:33 -0500

    adds MRMR logo screen

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuButtons : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(MRMRLogo());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator MRMRLogo()
+    {
+        yield return new WaitForSeconds(3);
+        transform.Find("MRMR Logo Background").GetComponent<Image>().enabled = false;
     }
 
     public void LoZWWButton()
@@ -31,4 +38,5 @@ public class MenuButtons : MonoBehaviour
     {
         SceneManager.LoadScene(3);
     }
+
 }
