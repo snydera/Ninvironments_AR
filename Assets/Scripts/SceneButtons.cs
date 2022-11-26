965313bdd0e8e01d8db936357dcf1527da239f02
Adam Snyder <adam555snyder@gmail.com>
2022-11-26 12:15:58 -0600

    adds fade screen animation

--- Assets/Scripts/SceneButtons.cs
+++ Assets/Scripts/SceneButtons.cs
@@ -8,17 +8,19 @@ public class SceneButtons : MonoBehaviour
 {
     [SerializeField] ARSession arSession;
     [SerializeField] Camera arCamera;
+    [SerializeField] Animator anim;
 
 
     private void Awake()
     {
         arCamera = Camera.main;
         arSession = arCamera.transform.parent.Find("AR Session").GetComponent<ARSession>();
+        anim = transform.Find("Fade Screen").GetComponent<Animator>();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        FadeIn();
     }
 
     // Update is called once per frame
@@ -36,5 +38,10 @@ public class SceneButtons : MonoBehaviour
     {
         arCamera.transform.localPosition = new Vector3(0, 0, 0);
         arSession.Reset();
+    }
+
+    public void FadeIn()
+    {
+        anim.Play("FadeIn");
     }
 }
