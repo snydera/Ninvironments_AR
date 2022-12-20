dceafabdba9ba832314872c32c3b478784be6958
Adam Snyder <adam555snyder@gmail.com>
2022-12-19 23:54:22 -0600

    adds to Logo Local concept

--- Assets/Scripts/AppLaunch.cs
+++ Assets/Scripts/AppLaunch.cs
@@ -6,28 +6,77 @@ using UnityEngine.UI;
 
 public class AppLaunch : MonoBehaviour
 {
+    //[SerializeField] ScrollRect scrollRect;
+    [SerializeField] GameObject continueButton;
+    [SerializeField] GameObject MRMRLogoBackground;
+    [SerializeField] GameObject appInfoBackground;
+
+    //[SerializeField] bool hasScrolledToBottom = false;
+
+    
+
+
+    private void Awake()
+    {
+        //scrollRect = transform.Find("Logo Local Background").Find("Scroll Object").GetComponent<ScrollRect>();
+        //scrollRect = transform.Find("Logo Local Background").Find("Scroll Object (original)").GetComponent<ScrollRect>();
+
+        //continueButton = transform.Find("Logo Local Background").Find("Continue Button").gameObject;
+        //continueButton = transform.Find("Logo Local Background").Find("Scroll Object").Find("Logo Panel").Find("Continue Button").gameObject;
+        MRMRLogoBackground = transform.Find("MRMR Logo Background").gameObject;
+        appInfoBackground = transform.Find("App Info Background").gameObject;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        //continueButton.SetActive(false);
         StartCoroutine(MRMRLogo());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //ScrollComplete();
     }
 
     IEnumerator MRMRLogo()
     {
         yield return new WaitForSeconds(3);
-        transform.Find("MRMR Logo Background").GetComponent<Image>().enabled = false;
-        StartCoroutine(LoadMainMenu());
+        MRMRLogoBackground.SetActive(false);
+        StartCoroutine(AppInfo());
     }
 
-    IEnumerator LoadMainMenu()
+    IEnumerator AppInfo()
     {
         yield return new WaitForSeconds(5);
+        appInfoBackground.SetActive(false);
+    }
+
+    /*public void ScrollComplete()
+    {
+        if (scrollRect.verticalNormalizedPosition <= 0.1f)
+        {
+            hasScrolledToBottom = true;
+        }
+    }*/
+
+    /*public float ScrollComplete
+    {
+        
+    }*/
+
+    /*public void EnableContinueButton()
+    {
+        if (hasScrolledToBottom == true)
+        {
+            continueButton.SetActive(true);
+        }
+    }*/
+
+    public void LoadMainMenu()
+    {
+        Debug.Log("Button Clicked");
         SceneManager.LoadScene(1);
     }
 }
